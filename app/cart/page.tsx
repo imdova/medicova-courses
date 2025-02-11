d6e57781c3cdae99840d7e161abc5a128037f994
AbdElRahman501 <abdelrahman.6182@gmail.com>
2025-02-11 12:07:28 +0200

    fawry pay test

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -4,18 +4,98 @@ import { PaymentForm } from "@/components/cart/PaymentForm";
 import { CartFormValues, CartItem } from "@/types/cart";
 import { useState } from "react";
 
+const cartItems: CartItem[] = [
+  {
+    id: "1",
+    title: "adipising elit, sed do eiusmod tempor",
+    description: "Lorem ipsum dollar...",
+    price: 24.69,
+    image:
+      "https://img.freepik.com/free-photo/focused-book-group-people-business-conference-modern-classroom-daytime_146671-16265.jpg?t=st=1739101586~exp=1739105186~hmac=24bd32da7f884be27d267b17e70060cd62895079067ae74431809e6fbc32aa29&w=996",
+  },
+  {
+    id: "2",
+    title: "adipising elit, sed do eiusmod tempor",
+    description: "Lorem ipsum dollar...",
+    price: 24.69,
+    image:
+      "https://img.freepik.com/free-photo/focused-book-group-people-business-conference-modern-classroom-daytime_146671-16265.jpg?t=st=1739101586~exp=1739105186~hmac=24bd32da7f884be27d267b17e70060cd62895079067ae74431809e6fbc32aa29&w=996",
+  },
+];
+
+const transaction_data = {
+  merchantCode: "1tSa6uxz2nTwlaAmt38enA==",
+  customerName: "example",
+  customerMobile: "01234567891",
+  customerEmail: "example@gmail.com",
+  customerProfileId: "777777",
+  cardNumber: "4242424242424242",
+  cardExpiryYear: "25",
+  cardExpiryMonth: "05",
+  cvv: "123",
+  merchantRefNum: "2312465464",
+  amount: 580.55,
+  currencyCode: "EGP",
+  language: "en-gb",
+  chargeItems: [
+    {
+      itemId: "897fa8e81be26df25db592e81c31c",
+      description: "Item Descriptoin",
+      price: 580.55,
+      quantity: "1",
+    },
+  ],
+  enable3DS: true,
+  authCaptureModePayment: false,
+  returnUrl: "https://developer.fawrystaging.com",
+  signature: "2ca4c078ab0d4c50ba90e31b3b0339d4d4ae5b32f97092dd9e9c07888c7eef36",
+  paymentMethod: "CARD",
+  description: "Example Description",
+};
+
 const CartPage = () => {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [submitError, setSubmitError] = useState<string | null>(null);
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const handleSubmit = async (data: CartFormValues) => {
-    setIsSubmitting(true);
-    setSubmitError(null);
+    const PaymentData = {
+      merchantCode: transaction_data.merchantCode,
+      customerName: transaction_data.customerName,
+      customerMobile: transaction_data.customerMobile,
+      customerEmail: transaction_data.customerEmail,
+      customerProfileId: transaction_data.customerProfileId,
+      cardNumber: transaction_data.cardNumber,
+      cardExpiryYear: transaction_data.cardExpiryYear,
+      cardExpiryMonth: transaction_data.cardExpiryMonth,
+      cvv: transaction_data.cvv,
+      merchantRefNum: transaction_data.merchantRefNum,
+      amount: transaction_data.amount,
+      currencyCode: transaction_data.currencyCode,
+      language: transaction_data.language,
+      chargeItems: transaction_data.chargeItems,
+      enable3DS: true,
+      authCaptureModePayment: false,
+      returnUrl: "https://developer.fawrystaging.com",
+      signature: transaction_data.signature,
+      paymentMethod: "PayUsingCC",
+      description: "transaction description",
+    };
 
     try {
-      // Simulate API call
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      console.log("Form Submitted:", data);
+      const response = await fetch(
+        "https://atfawry.fawrystaging.com/ECommerceWeb/Fawry/payments/charge",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(PaymentData),
+        }
+      );
+
+      const result = await response.json();
+      console.log("Form Submitted:", result);
       // Handle successful submission (e.g., show success toast, redirect)
     } catch (error) {
       setSubmitError(
@@ -26,25 +106,6 @@ const CartPage = () => {
       setIsSubmitting(false);
     }
   };
-
-  const cartItems: CartItem[] = [
-    {
-      id: "1",
-      title: "adipising elit, sed do eiusmod tempor",
-      description: "Lorem ipsum dollar...",
-      price: 24.69,
-      image:
-        "https://img.freepik.com/free-photo/focused-book-group-people-business-conference-modern-classroom-daytime_146671-16265.jpg?t=st=1739101586~exp=1739105186~hmac=24bd32da7f884be27d267b17e70060cd62895079067ae74431809e6fbc32aa29&w=996",
-    },
-    {
-      id: "2",
-      title: "adipising elit, sed do eiusmod tempor",
-      description: "Lorem ipsum dollar...",
-      price: 24.69,
-      image:
-        "https://img.freepik.com/free-photo/focused-book-group-people-business-conference-modern-classroom-daytime_146671-16265.jpg?t=st=1739101586~exp=1739105186~hmac=24bd32da7f884be27d267b17e70060cd62895079067ae74431809e6fbc32aa29&w=996",
-    },
-  ];
 
   return (
     <main className="mb-20">
